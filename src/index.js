b63850bb16a92f427af1899ba32efaeffb34b5d7
Lulala Chen <lulalachen@gmail.com>
2020-04-24 04:01:53 -0700

    feat(1): finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,98 @@
 import { ApolloServer } from 'apollo-server'
 
-const typeDefs = `
+const doctors = [
+  {
+    uuid: '111',
+    name: 'John Smith',
+    priceFairness: 'LOW',
+    addresses: [
+      {
+        street1: '2381 Rosecrans',
+        zipCode: '90245',
+      },
+    ],
+  },
+  {
+    uuid: '222',
+    name: 'Sally Smith',
+    priceFairness: 'HIGH',
+    addresses: [
+      {
+        street1: '1234 Rosecrans',
+        zipCode: '90245',
+      },
+    ],
+  },
+  {
+    uuid: '333',
+    name: 'Fluffy Smith',
+    priceFairness: 'LOW',
+    addresses: [],
+  },
+]
 
+const typeDefs = `
+  type Query {
+    getDoctor(input: GetDoctorInput): Doctor
+    getDoctorList(input: GetDoctorListInput): [Doctor]
+  }
+
+  type Mutation {
+    bookmarkDoctor(input: GetDoctorInput): Doctor
+  }
+
+  # --------- #
+
+  type Doctor {
+    uuid: ID!
+    name: String!
+    addresses: [Address]
+    priceFairness: PriceFairness
+  }
+
+  enum PriceFairness {
+    LOW
+    FAIR
+    HIGH
+  }
+
+  type Address {
+    street1: String
+    street2: String
+    zipCode: String!
+  }
+
+  # --------- #
+
+  input GetDoctorInput {
+    " uuid of doctor "
+    uuid: ID!
+  }
+
+  input GetDoctorListInput {
+    " filter by the name of doctor "
+    name: String
+    " filter by the price fairness of the doctor "
+    priceFairness: PriceFairness
+  }
 `
 
-const resolvers = {}
+const resolvers = {
+  Query: {
+    getDoctor: (_parent, { input = {} }, _context, _info) => {
+      return doctors.find(doc => doc.uuid === input.uuid)
+    },
+    getDoctorList: (_parent, { input = {} }, _context, _info) => {
+      return doctors.filter(
+        doc =>
+          (!!input.name ? doc.name === input.name : true) &&
+          (!!input.priceFairness
+            ? doc.priceFairness === input.priceFairness
+            : true),
+      )
+    },
+  },
+}
 
 const server = new ApolloServer({
   typeDefs,
